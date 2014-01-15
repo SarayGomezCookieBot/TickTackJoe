44b1d8e63c9919775d11653083d603370a30b5bc
Saray Gómez <cookiebotstudio@gmail.com>
2014-01-15 12:45:13 +0100

    Initial Code

---
+++ src/com/example/ticktackjoe/TickTackJoeMainActivity.java
@@ -0,0 +1,22 @@
+package com.example.ticktackjoe;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class TickTackJoeMainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_tick_tack_joe_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.tick_tack_joe_main, menu);
+		return true;
+	}
+
+}
